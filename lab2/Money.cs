02f9db970f672438e797f400011a073474486c4c
Михаил Кошурников <kjgug1027@gmail.com>
2022-12-18 17:19:50 +0500

    Добавьте файлы проекта.

---
+++ lab2/Money.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace lab2
+{
+    class Money
+    {
+        uint rubles;
+        byte kopeks;
+
+        public Money()// конструктор без параметров 
+        {
+            this.rubles = 0;
+            this.kopeks = 0;
+        }
+
+        public Money(uint rubles, byte kopeks)// конструктор с параметрами
+        {
+            this.Rubles = rubles;
+            this.Kopeks = kopeks;
+        }
+
+        public uint Rubles // свойство для доступа к полю rubles
+        {
+            get { return this.rubles; }
+            set {
+                if (value < 0)
+                    throw new Exception("Ошибка! Введено отрицательное значение!");
+                this.rubles = value; 
+            }
+        }
+
+        public byte Kopeks // свойство для доступа к полю kopeks
+        {
+            get { return this.kopeks; }
+            set {
+                if (value < 0)
+                    throw new Exception("Ошибка! Введено отрицательное значение!");
+                else if (value >= 100)
+                {
+                    rubles++;
+                    this.kopeks = (byte)(value - 100);
+                }
+                else this.kopeks = value;
+             }
+        }
+
+        public override string ToString() // переопределение функции ToString
+        {
+            return "Рубли: " + this.rubles+ "; Копейки: " + this.kopeks + ";";
+        }
+
+        public Money Minus(Money money) // метод вычитающий объект типа money из вызывающего объекта
+        {
+            Money res;
+            if ((this.rubles - money.rubles) < 0)
+                throw new Exception("Ошибка! Отрицательное значение!");
+            if (this.kopeks < money.kopeks)
+            {
+                this.rubles--;
+                res = new Money(this.rubles - money.rubles, (byte)(100 + this.kopeks - money.kopeks));
+            }
+            else res = new Money(this.rubles - money.rubles, (byte)(this.kopeks - money.kopeks));
+            return res;
+        }
+
+        public static Money operator ++(Money money) // увеличение копеек на еденицу
+        {
+            money.Kopeks++;
+            return money;
+        }
+
+        public static Money operator --(Money money) // уменьшение копеек на еденицу
+        {
+            if (money.Kopeks == 0)
+            {
+                money.Rubles--;
+                money.Kopeks = 100 - 1;
+                return money;
+            }
+            money.Kopeks--;
+            return money;
+        }
+
+        public static implicit operator uint(Money money) // неявное преобразование к uint
+        {
+            return money.rubles;
+        }
+
+        public static explicit operator double(Money money) // явное преобразование к double
+        {
+            return (double)money.Kopeks/100;
+        }
+
+        public static Money operator-(Money m, uint n) // оператор вычитания из объекта тип Money целого числа без знака
+        {
+            if (m.Rubles < n)
+                throw new Exception("Ошибка! Отрицательное значение!");
+            Money res = new Money(m.rubles - n, m.kopeks);
+            return res;
+        }
+
+        public static Money operator -(uint n, Money m) // оператор вычитания из целого числа без знака объекта типа Money
+        {
+            Money res;
+            if (n < m.rubles)
+                throw new Exception("Ошибка! Отрицательное значение!");
+            if (m.kopeks != 0)
+            {
+                n--;
+                res = new Money(n - m.rubles, (byte)(100 - m.kopeks));
+            }
+            else 
+                res = new Money(n - m.rubles, m.kopeks);
+            return res;
+            
+        }
+
+        public static Money operator -(Money m1, Money m2) // оператор разности двух объектов типа Money
+        {
+            return m1.Minus(m2);
+        }
+    }
+}
---
+++ lab2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Money money1 = new Money(200, 20);//money1 инициализация
+                Console.WriteLine("Money1: ");
+                Console.WriteLine(money1.ToString());// вызываем тостринг, потому что, если не вызывать, то неявно приводится к типу uint и выводит только рубли
+                Console.Write("Money1++: ");
+                money1++;
+                Console.WriteLine(money1.ToString());
+                Console.Write("Money1--: ");
+                money1--;
+                Console.WriteLine(money1.ToString());
+                Console.WriteLine();
+
+                Money money2 = new Money();//money2 создание и инициализация конструктором без параметров
+                Console.WriteLine("Money2: ");
+                Console.WriteLine(money2.ToString());
+                Console.WriteLine();
+
+                Console.WriteLine("Money2 новые значения: "); // money2 ввод новых значений
+                Console.Write("Рубли: ");
+                money2.Rubles = uint.Parse(Console.ReadLine());
+                Console.Write("Копейки: ");
+                money2.Kopeks = byte.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                money1 = money1.Minus(money2); // демонстрация метода minus
+                Console.WriteLine("Money1 = Money1 - Money2: ");
+                Console.WriteLine(money1.ToString());
+                uint rub = money1; // неявное приведение к uint
+                Console.WriteLine("Рубли: " + rub);
+                double kop = (double)money1; // явное приведение к double
+                Console.WriteLine("Рубли: " + kop);
+                Console.WriteLine();
+
+                Money money3 = new Money(); // money3 создание и инициализация 
+                money3 = money1 - 50; // демонстрация правостороннего оператора ' - '
+                Console.WriteLine("Money3 = Money1 - 50: ");
+                Console.WriteLine(money3.ToString());
+                Console.WriteLine();
+                Console.WriteLine("Money3 = 300 - Money1: "); // демонстрация левостороннего оператора ' - '
+                money3 = 300 - money1;
+                Console.WriteLine(money3.ToString());
+                Console.WriteLine();
+
+                money3 = money3 - money1; // демонстрация оператора ' - ' 
+                Console.WriteLine("Money3 = Money3 - Money1: ");
+                Console.WriteLine(money3.ToString());
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);            
+            }
+        }
+    }
+}
